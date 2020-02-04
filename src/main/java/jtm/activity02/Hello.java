7a81fd3177b145405ceacb2f0e60e60c9e3b6cde
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-02-04 20:05:16 +0200

    Add project files

---
+++ src/main/java/jtm/activity02/Hello.java
@@ -0,0 +1,12 @@
+package jtm.activity02;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
+
+//    git init
+//    git remote add origin https://github.com/[JUSU_GIT_KONTA_NOSAUKUMS]/bootcamp.git
